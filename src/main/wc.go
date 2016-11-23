65d651b81cd5d9cf5e5d9633dc8d6987833cd67a
ryanfakir <ryanfakir@gmail.com>
2016-11-22 22:06:13 -0500

    add part one

--- src/main/wc.go
+++ src/main/wc.go
@@ -1,17 +1,33 @@
 package main
 
-import "os"
-import "fmt"
-import "mapreduce"
-import "container/list"
+import (
+	"container/list"
+	"fmt"
+	"mapreduce"
+	"os"
+	"strconv"
+	"strings"
+	"unicode"
+)
+
 // our simplified version of MapReduce does not supply a
 // key to the Map function, as in the paper; only a value,
 // which is a part of the input file contents
 func Map(value string) *list.List {
+	splitFunc := func(c rune) bool {
+		return !unicode.IsLetter(c)
+	}
+	words := strings.FieldsFunc(value, splitFunc)
+	res := list.New()
+	for _, el := range words {
+		res.PushBack(mapreduce.KeyValue{Key: el, Value: "1"})
+	}
+	return res
 }
 
 // iterate over list and add values
 func Reduce(key string, values *list.List) string {
+	return strconv.Itoa(values.Len())
 }
 
 // Can be run in 3 ways:
@@ -19,17 +35,17 @@ func Reduce(key string, values *list.List) string {
 // 2) Master (e.g., go run wc.go master x.txt localhost:7777)
 // 3) Worker (e.g., go run wc.go worker localhost:7777 localhost:7778 &)
 func main() {
-  if len(os.Args) != 4 {
-    fmt.Printf("%s: see usage comments in file\n", os.Args[0])
-  } else if os.Args[1] == "master" {
-    if os.Args[3] == "sequential" {
-      mapreduce.RunSingle(5, 3, os.Args[2], Map, Reduce)
-    } else {
-      mr := mapreduce.MakeMapReduce(5, 3, os.Args[2], os.Args[3])    
-      // Wait until MR is done
-      <- mr.DoneChannel
-    }
-  } else {
-    mapreduce.RunWorker(os.Args[2], os.Args[3], Map, Reduce, 100)
-  }
+	if len(os.Args) != 4 {
+		fmt.Printf("%s: see usage comments in file\n", os.Args[0])
+	} else if os.Args[1] == "master" {
+		if os.Args[3] == "sequential" {
+			mapreduce.RunSingle(5, 3, os.Args[2], Map, Reduce)
+		} else {
+			mr := mapreduce.MakeMapReduce(5, 3, os.Args[2], os.Args[3])
+			// Wait until MR is done
+			<-mr.DoneChannel
+		}
+	} else {
+		mapreduce.RunWorker(os.Args[2], os.Args[3], Map, Reduce, 100)
+	}
 }
